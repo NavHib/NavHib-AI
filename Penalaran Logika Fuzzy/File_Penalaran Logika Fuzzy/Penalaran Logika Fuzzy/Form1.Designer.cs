8bff7355f7b5a3600c78b2539937dce7102fef58
Nauval Hibrizi <134928511+NavHib@users.noreply.github.com>
2023-06-04 23:40:11 +0700

    Implementasi penalaran logika fuzzy_ visual studio
    Implementasi Penalaran logika fuzzy menggunakan software visual studio versi 2012. untuk membuka dan menjalankan program, download semua file, kemudian buka file tipe (.sln) Run atau jalankan program. isi nila A, nilai B, dan nilai C terlebih dahulu secara random (acak). setelah itu geser Scroll Bar dan klik button Baca untuk menampilkan hasil MIU tinggi, MIU sedang, MIU Rendah.

---
+++ Penalaran Logika Fuzzy/File_Penalaran Logika Fuzzy/Penalaran Logika Fuzzy/Form1.Designer.cs
@@ -0,0 +1,248 @@
+﻿namespace Penalaran_Logika_Fuzzy
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.Tb_A = new System.Windows.Forms.TextBox();
+            this.Tb_B = new System.Windows.Forms.TextBox();
+            this.Tb_C = new System.Windows.Forms.TextBox();
+            this.Tb_X = new System.Windows.Forms.TextBox();
+            this.Tb_miuT = new System.Windows.Forms.TextBox();
+            this.Tb_miuS = new System.Windows.Forms.TextBox();
+            this.Tb_miuR = new System.Windows.Forms.TextBox();
+            this.Bt = new System.Windows.Forms.Button();
+            this.Hs = new System.Windows.Forms.HScrollBar();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(36, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(227, 18);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "PENALARAN LOGIKA FUZZY";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.Color.Black;
+            this.label2.Location = new System.Drawing.Point(12, 51);
+            this.label2.Name = "label2";
+            this.label2.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.label2.Size = new System.Drawing.Size(43, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Nilai A :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 77);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(43, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Nilai B :";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 102);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(43, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Nilai C :";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 140);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(43, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Nilai X :";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(170, 51);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(71, 13);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "MIU Tinggi   :";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(170, 74);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(73, 13);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "MIU Sedang :";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(170, 102);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(74, 13);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "MIU Rendah :";
+            // 
+            // Tb_A
+            // 
+            this.Tb_A.Location = new System.Drawing.Point(69, 48);
+            this.Tb_A.Name = "Tb_A";
+            this.Tb_A.Size = new System.Drawing.Size(45, 20);
+            this.Tb_A.TabIndex = 8;
+            // 
+            // Tb_B
+            // 
+            this.Tb_B.Location = new System.Drawing.Point(69, 74);
+            this.Tb_B.Name = "Tb_B";
+            this.Tb_B.Size = new System.Drawing.Size(45, 20);
+            this.Tb_B.TabIndex = 9;
+            // 
+            // Tb_C
+            // 
+            this.Tb_C.Location = new System.Drawing.Point(69, 99);
+            this.Tb_C.Name = "Tb_C";
+            this.Tb_C.Size = new System.Drawing.Size(45, 20);
+            this.Tb_C.TabIndex = 10;
+            // 
+            // Tb_X
+            // 
+            this.Tb_X.Location = new System.Drawing.Point(69, 137);
+            this.Tb_X.Name = "Tb_X";
+            this.Tb_X.Size = new System.Drawing.Size(45, 20);
+            this.Tb_X.TabIndex = 11;
+            // 
+            // Tb_miuT
+            // 
+            this.Tb_miuT.Location = new System.Drawing.Point(249, 48);
+            this.Tb_miuT.Name = "Tb_miuT";
+            this.Tb_miuT.Size = new System.Drawing.Size(45, 20);
+            this.Tb_miuT.TabIndex = 12;
+            // 
+            // Tb_miuS
+            // 
+            this.Tb_miuS.Location = new System.Drawing.Point(249, 74);
+            this.Tb_miuS.Name = "Tb_miuS";
+            this.Tb_miuS.Size = new System.Drawing.Size(45, 20);
+            this.Tb_miuS.TabIndex = 13;
+            // 
+            // Tb_miuR
+            // 
+            this.Tb_miuR.Location = new System.Drawing.Point(249, 99);
+            this.Tb_miuR.Name = "Tb_miuR";
+            this.Tb_miuR.Size = new System.Drawing.Size(45, 20);
+            this.Tb_miuR.TabIndex = 14;
+            // 
+            // Bt
+            // 
+            this.Bt.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.Bt.Location = new System.Drawing.Point(219, 134);
+            this.Bt.Name = "Bt";
+            this.Bt.Size = new System.Drawing.Size(75, 23);
+            this.Bt.TabIndex = 15;
+            this.Bt.Text = "Baca";
+            this.Bt.UseVisualStyleBackColor = false;
+            this.Bt.Click += new System.EventHandler(this.Bt_Click);
+            // 
+            // Hs
+            // 
+            this.Hs.Location = new System.Drawing.Point(15, 172);
+            this.Hs.Maximum = 109;
+            this.Hs.Name = "Hs";
+            this.Hs.Size = new System.Drawing.Size(279, 28);
+            this.Hs.TabIndex = 16;
+            this.Hs.Scroll += new System.Windows.Forms.ScrollEventHandler(this.Hs_Scroll);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.ClientSize = new System.Drawing.Size(306, 217);
+            this.Controls.Add(this.Hs);
+            this.Controls.Add(this.Bt);
+            this.Controls.Add(this.Tb_miuR);
+            this.Controls.Add(this.Tb_miuS);
+            this.Controls.Add(this.Tb_miuT);
+            this.Controls.Add(this.Tb_X);
+            this.Controls.Add(this.Tb_C);
+            this.Controls.Add(this.Tb_B);
+            this.Controls.Add(this.Tb_A);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox Tb_A;
+        private System.Windows.Forms.TextBox Tb_B;
+        private System.Windows.Forms.TextBox Tb_C;
+        private System.Windows.Forms.TextBox Tb_X;
+        private System.Windows.Forms.TextBox Tb_miuT;
+        private System.Windows.Forms.TextBox Tb_miuS;
+        private System.Windows.Forms.TextBox Tb_miuR;
+        private System.Windows.Forms.Button Bt;
+        private System.Windows.Forms.HScrollBar Hs;
+    }
+}
+
---
+++ Penalaran Logika Fuzzy/File_Penalaran Logika Fuzzy/Penalaran Logika Fuzzy/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Penalaran_Logika_Fuzzy
+{
+    public partial class Form1 : Form
+    {
+        double a, b, c, x, MT, MS, MR;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Bt_Click(object sender, EventArgs e)
+        {
+            miuT();
+            miuS();
+            miuR();
+            Tb_miuT.Text = MT.ToString(); 
+            Tb_miuS.Text = MS.ToString();
+            Tb_miuR.Text = MR.ToString();
+        }
+
+        private void Hs_Scroll(object sender, ScrollEventArgs e)
+        {
+            Baca();
+            Tampil();
+        }
+        void Baca()
+        {
+            x = Hs.Value;
+            a = double.Parse(Tb_A.Text);
+            b = double.Parse(Tb_B.Text);
+            c = double.Parse(Tb_C.Text);        
+        }
+        void Tampil()
+        {
+            Tb_X.Text = x.ToString();
+        }
+        void miuS()
+        {
+            if (x > a & x < b)              
+                MS = (x - a) / (b - a);              
+            else if (x > b & x < c)        
+                MS = (c - x) / (c - b);                
+            else if (x <= a)                
+                MS = 0;              
+            else if (x >= c)              
+                MS = 0;              
+            else if (x == b)         
+                MS = 1;               
+        }
+       void miuR()
+        {
+            if (x > a & x < b)
+                MR = (b - x) / (b - a);           
+            else if (x > b & x < c)
+                MR = 0;           
+            else if (x <= a)
+                MR = 1;           
+            else if (x >= c)
+                MR = 0;           
+            else if (x == b)
+                MR = 0;           
+        }
+        void miuT()
+        {
+            if (x > a & x < b)
+                MT = 0;
+            else if (x > b & x < c)
+                MT = (x - b) / (c - b);
+            else if (x <= a)
+                MT = 0;
+            else if (x >= c)
+                MT = 1;
+            else if (x == b)
+                MT = 0;
+        }
+    }
+}
+    
+
